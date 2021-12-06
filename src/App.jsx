8be0423949c623cf3c3102c3663ba0c5470ec6d2
TANNER MECK <steve.meck246@gmail.com>
2021-12-06 11:12:49 -0800

    Router, switch, and route added to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import BookList from './views/Books/BookList'
 import BookDetail from './views/Books/BookDetail'
+import Home from './views/Books/Home'
 import './App.css'
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
+      <Router>
+        <h1>Library Catalog</h1>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/books" component={BookList} />
+        </Switch>
+      </Router>
     </main>
   )
 }
--- src/views/Books/Home.jsx
+++ src/views/Books/Home.jsx
@@ -1,7 +1,6 @@
 function Home() {
   return (
     <>
-      <h1>Library Catalog</h1>
       <h3>Welcome to Tanner's Library of Computer Science!</h3>
       <p>Check out the amazing selection of books in our catalog</p>
     </>
