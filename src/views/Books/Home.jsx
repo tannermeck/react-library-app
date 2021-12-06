93f8f91272629f4f415fb273e2d566a517be2e05
TANNER MECK <steve.meck246@gmail.com>
2021-12-06 11:16:08 -0800

    link added to homepage to get to list of books

--- src/views/Books/Home.jsx
+++ src/views/Books/Home.jsx
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom'
+
 function Home() {
   return (
     <>
       <h3>Welcome to Tanner's Library of Computer Science!</h3>
-      <p>Check out the amazing selection of books in our catalog</p>
+      <p>
+        Check out the amazing selection of books in our <Link to="/books">catalog</Link>
+      </p>
     </>
   )
 }
