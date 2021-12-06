18247f19f5f68075b57e212c0da625bd049870be
TANNER MECK <steve.meck246@gmail.com>
2021-12-06 11:05:25 -0800

    home page added

---
+++ src/views/Books/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <>
+      <h1>Library Catalog</h1>
+      <h3>Welcome to Tanner's Library of Computer Science!</h3>
+      <p>Check out the amazing selection of books in our catalog</p>
+    </>
+  )
+}
+export default Home
